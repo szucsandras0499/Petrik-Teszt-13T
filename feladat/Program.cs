adc2d7b616e3c256cdc13c401d4574934c249b93
Petrik Lajos Szg <info@petrik.hu>
2018-09-24 15:15:56 +0200

    Kesz a feladat tanar ur.

--- feladat/Program.cs
+++ feladat/Program.cs
@@ -10,8 +10,41 @@ namespace feladat
     {
         static void Main(string[] args)
         {
+            List<int> szamok = new List<int>();
+            Console.WriteLine("Kérem adja meg azt a számot, hogy mennyi darab számot irassak ki:");
+            int bekertszamok;
+            while(!(int.TryParse(Console.ReadLine(), out bekertszamok)))
+            {
+                Console.WriteLine("Számot adj meg.");
+            }
+            int max = 0;
+            for (int i = 1; i <= bekertszamok; i++){
+                Console.WriteLine("Kérem írja be a(z)" + i + "." + " számot.");
+                szamok.Add(int.Parse(Console.ReadLine()));
+                
+               
+            }
+            foreach(var s in szamok)
+            {
+                if(s > max)
+                {
+                    max = s;
+                }
+            }
+            Console.WriteLine("A legnagyobb szám: " + max);
+            int min = max;
+            foreach (var s in szamok)
+            {
+                if (s < min)
+                {
+                    min = s;
+                }
+            }
+            Console.WriteLine("A legkisebb szám: " + min);
 
+            
 
+            Console.ReadLine();
         }
     }
 }
